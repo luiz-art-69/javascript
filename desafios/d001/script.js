f5b630744fe9d751bc9e4ede916bb3d6660366f9
Luiz Guilherme <119805498+luiz-art-69@users.noreply.github.com>
2024-03-25 11:07:19 -0300

    .

---
+++ desafios/d001/script.js
@@ -0,0 +1,18 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var hora = 13
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'imagens/manha.jpg'
+        document.body.style.backgroundColor = 'rgb(214, 186, 107)'
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = 'imagens/tarde.jpg'
+        document.body.style.backgroundColor = 'rgb(235, 180, 97)'
+    } else {
+        img.src = 'imagens/noite.jpg'
+        document.body.style.backgroundColor = 'rgb(97, 92, 86)'
+    }
+}
--- exercicios/ex011/ex011.js
+++ exercicios/ex011/ex011.js
@@ -1,8 +1,9 @@
-var idade = 22
+var idade = 65
+console.log(`Você tem ${idade} anos.`)
 if (idade < 16){
     console.log('Não vota')
-} else if (idade < 18) {
+} else if (idade < 18 || idade > 65) {
     console.log('Voto opcional')
-} else if (idade >= 18)  {
+}else {
     console.log('Voto obrigatorio!')
 }
