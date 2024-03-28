ebcc43a1270eeb16540fb20f6fa2eb46bfa71fec
Luiz Guilherme <119805498+luiz-art-69@users.noreply.github.com>
2024-03-28 19:25:28 -0300

    .

--- desafios/d005/script.js
+++ desafios/d005/script.js
@@ -1,10 +1,25 @@
-var num = parseInt(document.getElementById('num').value)
-var lista = document.getElementById('selnum')
-lista.innerHTML = ''
-var res = document.getElementById('res')
-var valores = []
-
+var lista = document.getElementById('selnum');
+var res = document.getElementById('res');
+var valores = [];
 
 function adicionar() {
-  
+    var num = parseInt(document.getElementById('num').value);
+    
+    if (num <= 0 || num > 100 || num == "") {
+        alert('Digite um número entre 1 e 100!');
+        return;
+    }
+
+    var options = document.getElementById('selnum').options;
+    for (var i = 0; i < options.length; i++){
+        if (parseInt(options[i].value) === num){
+            alert('Esse número já foi adicionado!')
+            return;
+        }
+    }
+    
+    var novoOption = document.createElement("option");
+    novoOption.value = num;
+    novoOption.text = `O valor ${num} foi adicionado.`;
+    lista.appendChild(novoOption);
 }
