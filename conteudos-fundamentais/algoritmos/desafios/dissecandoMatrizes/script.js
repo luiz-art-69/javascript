81e7b7fbf98ebd47a5fcae6497a582101875afd4
Luiz Guilherme <119805498+luiz-art-69@users.noreply.github.com>
2024-04-11 07:52:17 -0300

    .

---
+++ conteudos-fundamentais/algoritmos/desafios/dissecandoMatrizes/script.js
@@ -0,0 +1,38 @@
+var matriz = []
+
+// for (var l = 0; l < 4; l++){
+//     var linha = []
+//     for(var c = 0; c < 4; c++){
+//         var valor = parseInt(prompt(`Digite um valor para a posição [${l}, ${c}]`))
+//         linha.push(valor)
+//     }
+//     matriz.push(linha)
+// }
+
+function mostrarMatriz() {
+    
+}
+
+function mostrarDP() {
+    console.log(matriz)
+    document.getElementById("L0C0").style.backgroundColor = "yellow";
+    document.getElementById("L1C1").style.backgroundColor = "yellow";
+    document.getElementById("L2C2").style.backgroundColor = "yellow";
+    document.getElementById("L3C3").style.backgroundColor = "yellow";
+}
+
+function mostrarTS() {
+    console.log(matriz)
+    document.getElementById("L0C0").style.backgroundColor = "yellow";
+    document.getElementById("L1C1").style.backgroundColor = "yellow";
+    document.getElementById("L2C2").style.backgroundColor = "yellow";
+    document.getElementById("L3C3").style.backgroundColor = "yellow";
+}
+
+function mostrarTI() {
+    console.log(matriz)
+    document.getElementById("L0C0").style.backgroundColor = "yellow";
+    document.getElementById("L1C1").style.backgroundColor = "yellow";
+    document.getElementById("L2C2").style.backgroundColor = "yellow";
+    document.getElementById("L3C3").style.backgroundColor = "yellow";
+}
